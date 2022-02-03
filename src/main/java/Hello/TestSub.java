2fc5d9f7b5829201c039b3427ebf4c3fe75e6faf
sr-sudipta <sudiptaroy.sr7@gmail.com>
2022-02-03 13:55:43 +0530

    Added a subtraction class

---
+++ src/main/java/Hello/TestSub.java
@@ -0,0 +1,12 @@
+package Hello;
+
+public class TestSub {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int num1 = 10;
+		int num2 = 30;
+		System.out.println("Subtraction:"+(num2-num1));
+	}
+
+}
